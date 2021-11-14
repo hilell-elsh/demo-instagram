403f88515072d96e66b316130ca59fd290bd372c
Shaked Shaul <shukeds@gmail.com>
2021-11-14 19:27:13 +0200

    added tags to feed posts

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -22,6 +22,7 @@ import Divider from '@mui/material/Divider';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Chip from '@mui/material/Chip';
+import FilterHdrIcon from '@mui/icons-material/FilterHdr';
 
 const ExpandMore = styled((props) => {
     const { expand, ...other } = props;
@@ -42,161 +43,158 @@ export default function Feed() {
   };
 
     return (
-        <Card sx={{ maxWidth: 1000}} >
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            Y
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="Yossi Cohen"
-        subheader="September 14, 2021"
-      />
-      <CardMedia
-        component="img"
-        image="https://images.unsplash.com/photo-1546587348-d12660c30c50?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bmF0dXJhbHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80"
-        alt="Paella dish"
-      />
-      <CardContent>
-      <Typography variant="h6" color="text.secondary">
-          Yossi Cohen
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like. This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. 
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        
-        <Chip icon={
-            <Tooltip title="Like">
-                <IconButton aria-label="like">
-                    <FavoriteIcon />
-                </IconButton>
-            </Tooltip>
-      } label="1,549 Likes" variant="outlined" />
-        
-        <Tooltip title="Share">
-            <IconButton aria-label="share">
-                <ShareIcon />
-            </IconButton>
-        </Tooltip>
-        <Tooltip title="Add Comment">
-            <IconButton aria-label="add comment">
-                <AddCommentIcon />
-            </IconButton>
-        </Tooltip>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </ExpandMore>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-        <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar sx={{ bgcolor: blue[500] }} alt="Avi Moshe" src="/static/images/avatar/1.jpg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Avi Moshe"
-          secondary={
-            <React.Fragment>
-              <Typography
-                sx={{ display: 'block' }}
-                component="span"
-                variant="body2"
-                color="text.primary"
-              >
-                September 17, 2021
-              </Typography>
-              {"I'll be in your neighborhood doing errands this…"}
-              
-                <Chip icon={
-                    <Tooltip title="Like">
-                    <IconButton aria-label="like">
-                        <FavoriteIcon />
+        <Card sx={{ maxWidth: 1080 , marginTop: 1}} >
+            <CardHeader
+                avatar={
+                    <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                    Y
+                    </Avatar>
+                }
+                action={
+                    <IconButton aria-label="settings">
+                        <MoreVertIcon />
                     </IconButton>
-                    </Tooltip>
-                } label="579 Likes" variant="outlined" />
-              
-            </React.Fragment>
-          }
-        />
-      </ListItem>
-      <Divider variant="inset" component="li" />
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar sx={{ bgcolor: orange[500] }} alt="Israel Israeli" src="/static/images/avatar/2.jpg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Israel Israeli"
-          secondary={
-            <React.Fragment>
-              <Typography
-                sx={{ display: 'block' }}
-                component="span"
-                variant="body2"
-                color="text.primary"
-              >
-                September 15, 2021
-              </Typography>
-              {"Wish I could come, but I'm out of town this…"}
-              
+                }
+                title="Yossi Cohen"
+                subheader="September 14, 2021"
+            />
+            <CardMedia
+                component="img"
+                image="https://images.unsplash.com/photo-1546587348-d12660c30c50?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bmF0dXJhbHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80"
+                alt="Paella dish"
+            />
+            <CardContent>
+                <Typography variant="h6" color="text.secondary">
+                Yossi Cohen
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                This impressive paella is a perfect party dish and a fun meal to cook
+                together with your guests. Add 1 cup of frozen peas along with the mussels,
+                if you like. This impressive paella is a perfect party dish and a fun meal to cook
+                together with your guests. 
+                </Typography>
+                <Chip sx={{ bgcolor: green[200] }} icon={<FilterHdrIcon />} label="Nature" />
+            </CardContent>
+            <CardActions disableSpacing>
                 <Chip icon={
                     <Tooltip title="Like">
                         <IconButton aria-label="like">
                             <FavoriteIcon />
                         </IconButton>
                     </Tooltip>
-                } label="134 Likes" variant="outlined" />
-            </React.Fragment>
-          }
-        />
-      </ListItem>
-      <Divider variant="inset" component="li" />
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar sx={{ bgcolor: green[500] }} alt="Dana Nama" src="/static/images/avatar/3.jpg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Dana Nama"
-          secondary={
-            <React.Fragment>
-              <Typography
-                sx={{ display: 'block' }}
-                component="span"
-                variant="body2"
-                color="text.primary"
-              >
-                September 14, 2021
-              </Typography>
-              {'Do you have Paris recommendations?'}
-                <Chip icon={
-                    <Tooltip title="Like">
-                        <IconButton aria-label="like">
-                            <FavoriteIcon />
-                        </IconButton>
-                    </Tooltip>
-                } label="149 Likes" variant="outlined" />
-            </React.Fragment>
-          }
-        />
-      </ListItem>
-    </List>
-        </CardContent>
-      </Collapse>
-    </Card>
+                } label="1,549 Likes" variant="outlined" />
+                <Tooltip title="Share">
+                    <IconButton aria-label="share">
+                        <ShareIcon />
+                    </IconButton>
+                </Tooltip>
+                <Tooltip title="Add Comment">
+                    <IconButton aria-label="add comment">
+                        <AddCommentIcon />
+                    </IconButton>
+                </Tooltip>
+                <ExpandMore
+                    expand={expanded}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                    aria-label="show more"
+                >
+                    <ExpandMoreIcon />
+                </ExpandMore>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                    <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+                        <ListItem alignItems="flex-start">
+                            <ListItemAvatar>
+                                <Avatar sx={{ bgcolor: blue[500] }} alt="Avi Moshe" src="/static/images/avatar/1.jpg" />
+                            </ListItemAvatar>
+                            <ListItemText
+                                primary="Avi Moshe"
+                                secondary={
+                                    <React.Fragment>
+                                        <Typography
+                                            sx={{ display: 'block' }}
+                                            component="span"
+                                            variant="body2"
+                                            color="text.primary"
+                                        >
+                                        September 17, 2021
+                                        </Typography>
+                                        {"I'll be in your neighborhood doing errands this…"}
+                
+                                        <Chip icon={
+                                            <Tooltip title="Like">
+                                                <IconButton aria-label="like">
+                                                    <FavoriteIcon />
+                                                </IconButton>
+                                            </Tooltip>
+                                        } label="579 Likes" variant="outlined" />
+                                    </React.Fragment>
+                                }
+                            />
+                        </ListItem>
+                        <Divider variant="inset" component="li" />
+                        <ListItem alignItems="flex-start">
+                            <ListItemAvatar>
+                                <Avatar sx={{ bgcolor: orange[500] }} alt="Israel Israeli" src="/static/images/avatar/2.jpg" />
+                            </ListItemAvatar>
+                            <ListItemText
+                                primary="Israel Israeli"
+                                secondary={
+                                    <React.Fragment>
+                                        <Typography
+                                            sx={{ display: 'block' }}
+                                            component="span"
+                                            variant="body2"
+                                            color="text.primary"
+                                        >
+                                        September 15, 2021
+                                        </Typography>
+                                        {"Wish I could come, but I'm out of town this…"}
+                                        <Chip icon={
+                                            <Tooltip title="Like">
+                                                <IconButton aria-label="like">
+                                                    <FavoriteIcon />
+                                                </IconButton>
+                                            </Tooltip>
+                                        } label="134 Likes" variant="outlined" />
+                                    </React.Fragment>
+                                }
+                            />
+                        </ListItem>
+                        <Divider variant="inset" component="li" />
+                        <ListItem alignItems="flex-start">
+                        <ListItemAvatar>
+                            <Avatar sx={{ bgcolor: green[500] }} alt="Dana Nama" src="/static/images/avatar/3.jpg" />
+                        </ListItemAvatar>
+                        <ListItemText
+                            primary="Dana Nama"
+                            secondary={
+                                <React.Fragment>
+                                    <Typography
+                                        sx={{ display: 'block' }}
+                                        component="span"
+                                        variant="body2"
+                                        color="text.primary"
+                                    >
+                                    September 14, 2021
+                                    </Typography>
+                                    {'Do you have Paris recommendations?'}
+                                    <Chip icon={
+                                        <Tooltip title="Like">
+                                            <IconButton aria-label="like">
+                                                <FavoriteIcon />
+                                            </IconButton>
+                                        </Tooltip>
+                                    } label="149 Likes" variant="outlined" />
+                                </React.Fragment>
+                            }
+                        />
+                        </ListItem>
+                    </List>
+                </CardContent>
+            </Collapse>
+        </Card>
         )
 }
--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -2,6 +2,10 @@ import Feed from "../components/Feed";
 
 export default function PageFeed() {
     return (
+        <div>
             <Feed />
+            <Feed />
+        </div>
+            
     )
 }
