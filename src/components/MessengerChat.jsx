d44c5a9a1b9701dcd3bca3830af5b0f67e1493bb
Shaked Shaul <shukeds@gmail.com>
2021-11-19 01:54:48 +0200

    added MessengerChat.jsx file

---
+++ src/components/MessengerChat.jsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemButton from '@mui/material/ListItemButton';
+import Avatar from '@mui/material/Avatar';
+import ImageIcon from '@mui/icons-material/Image';
+import WorkIcon from '@mui/icons-material/Work';
+import BeachAccessIcon from '@mui/icons-material/BeachAccess';
+import Divider from '@mui/material/Divider';
+import { red , green ,blue , orange , purple , lime, lightGreen , deepPurple , deepOrange , pink } from '@mui/material/colors';
+
+
+
+export default function MessengerChat() {
+    return (
+        <div >
+            
+        </div>
+    )
+}
