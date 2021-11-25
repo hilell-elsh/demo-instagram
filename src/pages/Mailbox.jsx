2c13a466061459314b91dc2c3affdf5ca55d8517
hilell-elsh <84779482+hilell-elsh@users.noreply.github.com>
2021-11-25 21:06:06 +0200

    Messenger (#15)
    * change setting.jsx to settings.jsx
    
    * added feed page
    
    * change Header file name in App file
    
    * remove align-tems: center in index.css
    
    * added tags to feed posts
    
    * split Feed component
    
    * added MessengerList.jsx file
    
    * added MessengerChat.jsx file
    
    * added MessengerList and MessengerChat components to Mailbox component
    
    * added badge for some avatars
    
    Co-authored-by: Shaked Shaul <shukeds@gmail.com>

--- src/pages/Mailbox.jsx
+++ src/pages/Mailbox.jsx
@@ -1,7 +1,16 @@
-
+import MessengerList from "../components/MessengerList"
+import MessengerChat from "../components/MessengerChat"
 
 export default function Mailbox() {
     return (
-        <p>this is Mailbox</p>
+        <div style={{display:'flex'}}>
+            <div style={{width:'30%', margin: "5px" }} >
+                <MessengerList />
+            </div>
+            <div  style={{width:'70%'}}   >
+            <MessengerChat />
+            </div>
+                
+        </div>
     )
 }
