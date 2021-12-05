e121cfb44ba6bd873e370f64562f943674b9d368
מאור לכיש <mlachish1@gmail.com>
2021-12-05 20:04:46 +0200

    wip

--- apps/front-end/src/services/user-data.js
+++ apps/front-end/src/services/user-data.js
@@ -1,8 +1,6 @@
 import { user } from '../ex-apis/user.js'
-import { users } from '../ex-apis/users'
 
-export function getUser(ID=1) {
-    const user = users.find(user => user.profile.ID === ID)
+export function getUser() {
     return user
         // fetch('../ex-apis/posts.json')
         // .then((response) => response.json())
