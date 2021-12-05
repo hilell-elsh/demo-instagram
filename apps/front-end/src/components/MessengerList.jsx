1a44be203ce0811e71018bc0593c7862d29baeb6
מאור לכיש <mlachish1@gmail.com>
2021-12-05 19:04:07 +0200

    wip

--- apps/front-end/src/components/MessengerList.jsx
+++ apps/front-end/src/components/MessengerList.jsx
@@ -1,104 +1,169 @@
 import * as React from 'react';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import ListItemButton from '@mui/material/ListItemButton';
-import Avatar from '@mui/material/Avatar';
-import ImageIcon from '@mui/icons-material/Image';
-import WorkIcon from '@mui/icons-material/Work';
-import BeachAccessIcon from '@mui/icons-material/BeachAccess';
-import Divider from '@mui/material/Divider';
-import Badge from '@mui/material/Badge';
-import { red , green ,blue , orange , purple , lime, lightGreen , deepPurple , deepOrange , pink } from '@mui/material/colors';
+import { 
+    List,
+    ListItem,
+    ListItemText,
+    ListItemAvatar,
+    ListItemButton,
+    Avatar,
+    Divider,
+    Badge,
+    Typography
+    } from '@mui/material';
+import MessengerItem from './MessengerItem'
 
 
-
-export default function MessengerList() {
+export default function MessengerList({massages, numOf=0}) {
+    console.log("render list...");
+    massages.map(massage => {console.log(massage)});
     return (
-        <List
-            sx={{
-            bgcolor: 'background.paper',
-            }}
-            component="nav" aria-label="main mailbox folders"
-        >
+        <nav>
+            {massages.map(massage => {return <MessengerItem key={massage.senderId} chatData={massage} />})}
+        </nav>
+        // <>
+        // // ********** try without MUI **************
+        //  <nav>
+        //     <div className="msgItem">
+        //         <Badge badgeContent={1} color="error" sx={{ display: 'inline', margin: '15px' }}>
+        //             <Avatar sx={{ bgcolor: orange[500], display: 'inline-block'}} alt="Matthew Kent" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvwN3Vd48KHkaScpcZvJnSNEE8slCoCa5RJw&usqp=CAU" />
+        //         </Badge>
+        //         <div className="msgText" style={{display: 'inline-block'}}>
+        //             <h5>
+        //                 Matthew Kent
+        //             </h5>
+        //             <p style={{
+        //                 color: "blue",
+        //                 whiteSpace: 'nowrap',
+        //                 width: "200px",
+        //                 overflow: 'hidden',
+        //                 textOverflow: 'ellipsis'
+        //                 }}>
+
+        //                 Long massage Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+        //             </p>
+        //         </div>
+        //     </div>
+        // </nav>
+        // {/* // ********** try without MUI ************** */}
+
+        // <br/>
+        // <br/>
+        // // ********** try with MUI **************
+        // <br/>
+        // <List
+        //     sx={{
+        //     bgcolor: 'background.paper',
+        //     }}
+        //     component="nav" aria-label="main mailbox folders"
+        // >
         
-            <ListItem>
-                <ListItemAvatar>
-                    <Badge  badgeContent={1} color="error">
-                        <Avatar sx={{ bgcolor: orange[500] }} alt="Matthew Kent" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvwN3Vd48KHkaScpcZvJnSNEE8slCoCa5RJw&usqp=CAU" />
-                    </Badge>
-                </ListItemAvatar>
-                <ListItemText primary="Matthew Kent" secondary="Hello, this is a message" />
-            </ListItem>
+        //     <ListItem
+        //         sx={{
+        //             height: "100px",
+        //             width: "1000px",
+        //             whiteSpace: 'nowrap',
+        //             overflow: 'hidden',
+        //             textOverflow: 'ellipsis'
 
-            <Divider variant="inset" component="li" />
-            <ListItem>
-                <ListItemAvatar>
-                    <Avatar sx={{ bgcolor: green[500] }} alt="Aliyah Hester" src="/static/images/avatar/2.jpg" /> 
-                </ListItemAvatar>
-                <ListItemText primary="Aliyah Hester" secondary="Hello, this is a message" />
-            </ListItem>
-            <Divider variant="inset" component="li" />
-            <ListItem>
+        //         }}
+        //         alignItems = "flex-start"
+        //     >
+        //         <ListItemAvatar>
+        //             <Badge  badgeContent={1} color="error">
+        //                 <Avatar sx={{ bgcolor: orange[500] }} alt="Matthew Kent" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvwN3Vd48KHkaScpcZvJnSNEE8slCoCa5RJw&usqp=CAU" />
+        //             </Badge>
+        //         </ListItemAvatar>
+        //         <ListItemText 
+        //             primary="Matthew Kent" 
+        //             secondary={
+        //                 <Typography
+        //                 sx={{ 
+        //                     // display: 'inline',
+        //                     // whiteSpace: 'nowrap',
+        //                     overflowX: 'hidden',
+        //                     textOverflow: 'ellipsis',
+        //                     //  height: "20px",
+        //                     width: "100px",
+        //                     // component="span"
+        //                 }}
+        //                 >
+        //                     Long massage Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+        //                 </Typography>
+        //             }
+        //         />
+        //     </ListItem>
+
+        //     <Divider variant="inset" component="li" />
+        //     <ListItem>
+        //         <ListItemAvatar>
+        //             <Avatar sx={{ bgcolor: green[500] }} alt="Aliyah Hester" src="/static/images/avatar/2.jpg" /> 
+        //         </ListItemAvatar>
+        //         <ListItemText primary="Aliyah Hester" secondary="Hello, this is a message" />
+        //     </ListItem>
+        //     <Divider variant="inset" component="li" />
+        //     <ListItem>
                 
-                <ListItemAvatar>
-                    <Badge  badgeContent={1} color="error">
-                        <Avatar sx={{ bgcolor: blue[500] }} alt="Micheal Bright" src="/static/images/avatar/2.jpg" />
-                    </Badge>
-                </ListItemAvatar>
-                <ListItemText primary="Micheal Bright" secondary="Hello, this is a message" />
-            </ListItem>
-            <Divider variant="inset" component="li" />
-            <ListItem>
-                <ListItemAvatar>
-                    <Avatar sx={{ bgcolor: red[500] }} alt="Garin Ireland" src="/static/images/avatar/2.jpg" /> 
-                </ListItemAvatar>
-                <ListItemText primary="Garin Ireland" secondary="Hello, this is a message" />
-            </ListItem>
-            <Divider variant="inset" component="li" />
-            <ListItem>
-                <ListItemAvatar>
-                    <Avatar sx={{ bgcolor: purple[500] }} alt="Hilda Flower" src="/static/images/avatar/2.jpg" /> 
-                </ListItemAvatar>
-                <ListItemText primary="Hilda Flower" secondary="Hello, this is a message" />
-            </ListItem>
-            <Divider variant="inset" component="li" />
-            <ListItem>
-                <ListItemAvatar>
-                    <Avatar sx={{ bgcolor: lime[500] }} alt="Reema Mcguire" src="/static/images/avatar/2.jpg" /> 
-                </ListItemAvatar>
-                <ListItemText primary="Reema Mcguire" secondary="Hello, this is a message" />
-            </ListItem>
-            <Divider variant="inset" component="li" />
-            <ListItem>
-                <ListItemAvatar>
-                    <Avatar sx={{ bgcolor: lightGreen[500] }} alt="Kelsea Carrillo" src="/static/images/avatar/2.jpg" /> 
-                </ListItemAvatar>
-                <ListItemText primary="Kelsea Carrillo" secondary="Hello, this is a message" />
-            </ListItem>
-            <Divider variant="inset" component="li" />
-            <ListItem>
-                <ListItemAvatar>
-                    <Avatar sx={{ bgcolor: deepPurple[500] }} alt="Issac Howard" src="/static/images/avatar/2.jpg" /> 
-                </ListItemAvatar>
-                <ListItemText primary="Issac Howard" secondary="Hello, this is a message" />
-            </ListItem>
-            <Divider variant="inset" component="li" />
-            <ListItem>
-                <ListItemAvatar>
-                    <Avatar sx={{ bgcolor: deepOrange[500] }} alt="Florrie Mendez" src="/static/images/avatar/2.jpg" /> 
-                </ListItemAvatar>
-                <ListItemText primary="Florrie Mendez" secondary="Hello, this is a message" />
-            </ListItem>
-            <Divider variant="inset" component="li" />
-            <ListItem>
-                <ListItemAvatar>
-                    <Avatar sx={{ bgcolor: pink[500] }} alt="Matilda Cook" src="/static/images/avatar/2.jpg" /> 
-                </ListItemAvatar>
-                <ListItemText primary="Matilda Cook" secondary="Hello, this is a message" />
-            </ListItem>
+        //         <ListItemAvatar>
+        //             <Badge  badgeContent={1} color="error">
+        //                 <Avatar sx={{ bgcolor: blue[500] }} alt="Micheal Bright" src="/static/images/avatar/2.jpg" />
+        //             </Badge>
+        //         </ListItemAvatar>
+        //         <ListItemText primary="Micheal Bright" secondary="Hello, this is a message" />
+        //     </ListItem>
+        //     <Divider variant="inset" component="li" />
+        //     <ListItem>
+        //         <ListItemAvatar>
+        //             <Avatar sx={{ bgcolor: red[500] }} alt="Garin Ireland" src="/static/images/avatar/2.jpg" /> 
+        //         </ListItemAvatar>
+        //         <ListItemText primary="Garin Ireland" secondary="Hello, this is a message" />
+        //     </ListItem>
+        //     <Divider variant="inset" component="li" />
+        //     <ListItem>
+        //         <ListItemAvatar>
+        //             <Avatar sx={{ bgcolor: purple[500] }} alt="Hilda Flower" src="/static/images/avatar/2.jpg" /> 
+        //         </ListItemAvatar>
+        //         <ListItemText primary="Hilda Flower" secondary="Hello, this is a message" />
+        //     </ListItem>
+        //     <Divider variant="inset" component="li" />
+        //     <ListItem>
+        //         <ListItemAvatar>
+        //             <Avatar sx={{ bgcolor: lime[500] }} alt="Reema Mcguire" src="/static/images/avatar/2.jpg" /> 
+        //         </ListItemAvatar>
+        //         <ListItemText primary="Reema Mcguire" secondary="Hello, this is a message" />
+        //     </ListItem>
+        //     <Divider variant="inset" component="li" />
+        //     <ListItem>
+        //         <ListItemAvatar>
+        //             <Avatar sx={{ bgcolor: lightGreen[500] }} alt="Kelsea Carrillo" src="/static/images/avatar/2.jpg" /> 
+        //         </ListItemAvatar>
+        //         <ListItemText primary="Kelsea Carrillo" secondary="Hello, this is a message" />
+        //     </ListItem>
+        //     <Divider variant="inset" component="li" />
+        //     <ListItem>
+        //         <ListItemAvatar>
+        //             <Avatar sx={{ bgcolor: deepPurple[500] }} alt="Issac Howard" src="/static/images/avatar/2.jpg" /> 
+        //         </ListItemAvatar>
+        //         <ListItemText primary="Issac Howard" secondary="Hello, this is a message" />
+        //     </ListItem>
+        //     <Divider variant="inset" component="li" />
+        //     <ListItem>
+        //         <ListItemAvatar>
+        //             <Avatar sx={{ bgcolor: deepOrange[500] }} alt="Florrie Mendez" src="/static/images/avatar/2.jpg" /> 
+        //         </ListItemAvatar>
+        //         <ListItemText primary="Florrie Mendez" secondary="Hello, this is a message" />
+        //     </ListItem>
+        //     <Divider variant="inset" component="li" />
+        //     <ListItem>
+        //         <ListItemAvatar>
+        //             <Avatar sx={{ bgcolor: pink[500] }} alt="Matilda Cook" src="/static/images/avatar/2.jpg" /> 
+        //         </ListItemAvatar>
+        //         <ListItemText primary="Matilda Cook" secondary="Hello, this is a message" />
+        //     </ListItem>
 
-        </List>
+        // </List>
+        // {/* // ********** try without MUI ************** */}
+        // </>
     )
 }
+
+
--- apps/front-end/src/ex-apis/messages.js
+++ apps/front-end/src/ex-apis/messages.js
@@ -1,31 +1,66 @@
 export const messages = [
     {
-        "massage": "hello world",
-        "sender": "{sombody.username}",
-        "time": {
-            "date": "28.10.21",
-            "hour": "14:35:19"
-        },
-        "readed": "true"
+        "senderId": 3,
+        "massages": [
+            {
+                "massage": "hello to you",
+                "sender": "{sombody.username}",
+                "time": {
+                    "date": "28.10.21",
+                    "hour": "14:35:19"
+                },
+                "readed": "false"
+            },     
+            {
+                "massage": "hi to you",
+                "sender": "{sombody.username}",
+                "time": {
+                    "date": "28.10.21",
+                    "hour": "10:44:58"
+                },
+                "readed": "false"
+            },        
+            {
+                "massage": "hi to you",
+                "sender": "{sombody.username}",
+                "time": {
+                    "date": "28.10.21",
+                    "hour": "10:44:58"
+                },
+                "readed": "true"
+            }
+        ]
     },
-
     {
-        "massage": "hi to you",
-        "sender": "{sombody.username}",
-        "time": {
-            "date": "28.10.21",
-            "hour": "10:44:58"
-        },
-        "readed": "false"
-    },
-    
-    {
-        "massage": "hi to you",
-        "sender": "{sombody.username}",
-        "time": {
-            "date": "28.10.21",
-            "hour": "10:44:58"
-        },
-        "readed": "false"
+        "senderId": 2,
+        "massages": [
+            {
+                "massage": "success!! we try and success to to this message item!",
+                "sender": "{sombody.username}",
+                "time": {
+                    "date": "28.10.21",
+                    "hour": "14:35:19"
+                },
+                "readed": "false"
+            },     
+            {
+                "massage": "hi to you",
+                "sender": "{sombody.username}",
+                "time": {
+                    "date": "28.10.21",
+                    "hour": "10:44:58"
+                },
+                "readed": "true"
+            },        
+            {
+                "massage": "hi to you",
+                "sender": "{sombody.username}",
+                "time": {
+                    "date": "28.10.21",
+                    "hour": "10:44:58"
+                },
+                "readed": "true"
+            }
+        ]
     }
 ]
