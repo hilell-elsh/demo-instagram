9f492c683b0549e36198a45913231ab85324f76b
Shaked Shaul <shukeds@gmail.com>
2021-12-08 20:10:20 +0200

    fix

--- apps/back-end/index.js
+++ apps/back-end/index.js
@@ -4,6 +4,6 @@ const {join} = require('path')
 const app = express()
 app.use("/",express.static(join(__dirname,"static")))
 app.use("*",express.static(join(__dirname,"static/index.html")))
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log('listening on port 3000')
 })
