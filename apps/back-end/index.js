8c82ff064d75960285370c155191826fd060da5d
Hilell Elshalem <hilell.elshalem@gmail.com>
2021-12-09 20:00:18 +0200

    init branch

--- apps/back-end/index.js
+++ apps/back-end/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const {join} = require('path')
 
 const app = express()
-app.use("/",express.static(join(__dirname,"static")))
-app.use("*",express.static(join(__dirname,"static/index.html")))
+app.use(express.json());
+app.use("/",express.static(join(__dirname,"static")));
+app.use("*",express.static(join(__dirname,"static/index.html")));
 app.listen(process.env.PORT || 3000, function () {
     console.log('listening on port 3000')
 })
