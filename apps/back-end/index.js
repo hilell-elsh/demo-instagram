4a629de488b12c1c44dea8f8668c7f48635f6cb1
Hilell Elshalem <hilell.elshalem@gmail.com>
2021-12-09 21:38:42 +0200

    list of nesting db & server routes

--- apps/back-end/index.js
+++ apps/back-end/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const {join} = require('path')
-
+const DEAFULT_PORT = 44444
 const app = express()
 app.use(express.json());
 app.use("/",express.static(join(__dirname,"static")));
 app.use("*",express.static(join(__dirname,"static/index.html")));
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || DEAFULT_PORT, function () {
     console.log('listening on port 3000')
 })
