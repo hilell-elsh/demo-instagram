34440cc4e0e9b27dd4c97dd5e0a9e3442d989ac1
מאור לכיש <mlachish1@gmail.com>
2021-12-12 22:24:48 +0200

    wtf

--- apps/front-end/src/components/profile/ProfileInfo.jsx
+++ apps/front-end/src/components/profile/ProfileInfo.jsx
@@ -4,7 +4,7 @@ import Avatar from '@mui/material/Avatar'
 
 import { getUser } from '../../services/user-data'
 import { Username, InfoWrapper, Button, Count } from './ProfileStyle'
-import { modalOpen } from '../../store/modal'
+import { modalOpen, modalClose } from '../../store/modal'
 import FollowerModal from './FollowerModal'
 
 const userProfile = getUser().profile
@@ -17,19 +17,28 @@ const userAvatar = <Avatar
                         
 export default function ProfileInfo() {
     
-    const isModal = useSelector((state) => state.modal.isModal)
-    const title = useSelector((state) => state.modal.title)
+    const {isModal} = useSelector((state) => state.modal)
     const dispatch = useDispatch()
+
+    // const [title, setTitle] = useState('')
     
+    // const handleOpen = () => {
+    //     setDisplay(true);
+    //     setTitle(event.target.innerText)
+    // }
+
+    // const handleClose = () => setDisplay(false);
+    // -----------------------------------------
+
     return (
     <>
         <InfoWrapper>
             {userAvatar}
             <Username>{userProfile.username}</Username>
             <Count>{userProfile.posts.myPosts.total} <span>posts</span></Count>
-            <Count>{userProfile.followers.total} <span onClick={() => dispatch(modalOpen())}>followers</span></Count>
-            <Count>{userProfile.following.total} <span onClick={() => dispatch(modalOpen())}>following</span></Count>
-            { isModal && <FollowerModal title={title}/>}
+            <Count>{userProfile.followers.total} <span onClick={() => dispatch(modalOpen)}>followers</span></Count>
+            <Count>{userProfile.following.total} <span onClick={() => dispatch(modalOpen)}>following</span></Count>
+            { isModal && <FollowerModal handleClose={modalClose} modal={modal} title={title}/>}
             <Link to="/:username/settings">
                 <Button>Edit Profile</Button>
             </Link>
--- apps/front-end/src/ex-apis/user.js
+++ apps/front-end/src/ex-apis/user.js
@@ -17,7 +17,12 @@ export const user = {
             "src": "https://res.cloudinary.com/teepublic/image/private/s--9ZtubWhB--/t_Resized%20Artwork/c_fit,g_north_west,h_1054,w_1054/co_ffffff,e_outline:53/co_ffffff,e_outline:inner_fill:53/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_auto,h_630,q_90,w_630/v1563682998/production/designs/5373105_0.jpg"            
         },
         "followers": {
-            "total": "43"
+            "total": "43",
+            "usernames": [
+                "avi",
+                "moshe", 
+                "yossi"
+            ]
         },
         "following": {
             "total": "50"
