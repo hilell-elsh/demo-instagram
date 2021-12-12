ff26c1e33f163561632f6d1f61c5e7c4a591b833
מאור לכיש <mlachish1@gmail.com>
2021-12-12 22:23:01 +0200

    wtf

--- apps/front-end/src/components/profile/FollowerModal.jsx
+++ apps/front-end/src/components/profile/FollowerModal.jsx
@@ -1,36 +1,18 @@
-import { useDispatch } from 'react-redux'
-import Avatar from '@mui/material/Avatar'
-
-import { ModalBackdrop, ModalWrapper, ModalHeader, ModalContent, ModalContentItem } from '../global/ModalStyle'
-import { getUser } from '../../services/user-data'
-import { modalClose } from '../../store/modal'
+import {ModalBackdrop, ModalWrapper, ModalHeader, ModalContent} from '../global/ModalStyle'
 
 export default function FollowerModal(props) {
 
-    const userFollowers = getUser().profile.followers.usernames
-    const dispatch = useDispatch()
-    
     return(
     <ModalBackdrop>
         <ModalWrapper>
             <ModalHeader>
                 <p>{props.title}</p>
-                <i className="fas fa-times" onClick={() => dispatch(modalClose())}></i>
+                <i className="fas fa-times" onClick={props.handleClose}></i>
             </ModalHeader>
             <ModalContent>
-                {userFollowers.map((username, index) => (
-                    <ModalContentItem key={index}>
-                        <Avatar src={username.image.src} sx={{ width: 40, height: 40 }}/>
-                        {/* add link with username */}
-                        <p>{username.name.firstName + " " + username.name.lastName}</p>
-                        {/* add remove button */}
-                    </ModalContentItem>
-                ))}
+            
             </ModalContent>
         </ModalWrapper>
     </ModalBackdrop>
     )
 }
-
-// modal content items needs to come from the api that ha the same name as the modal title
-// separate content to followers and following and implement each component according to the modal title
--- apps/front-end/src/pages/Settings.jsx
+++ apps/front-end/src/pages/Settings.jsx
@@ -1,7 +1,7 @@
-import EditProfile from "../components/profile/EditProfile"
+
 
 export default function Settings() {
     return (
-        <EditProfile />
+        <p>this is Settings</p>
     )
 }
