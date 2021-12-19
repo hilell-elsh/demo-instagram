78fe6490b11f8066fe6e9291a76b10b851c6f0ab
Hilell Elshalem <hilell.elshalem@gmail.com>
2021-12-19 22:06:42 +0200

    add services

---
+++ apps/back-end/services/tags.js
@@ -0,0 +1,30 @@
+const TagModel = require('../models/tag');
+
+function createTag (data) {
+    const newTag = new TagModel(data);
+    return newTag.save();
+}
+
+function getTag(query={}) {
+    return TagModel.findOne({ query })    
+}
+
+function getTags(query={}) {
+    return TagModel.find({ query })
+}
+
+function deleteTag(query={}) {
+    return TagModel.findOneAndDelete({ query })    
+}
+
+function updateTag(query={}, data) {
+    return TagModel.findOneAndUpdate(query, data)
+}
+
+module.exports = {
+    createTag,
+    getTag,
+    getTags,
+    deleteTag,
+    updateTag
+};
