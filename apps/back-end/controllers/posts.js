d8d4965f80f1db8625d586913fd66a7f133ecfe0
Hilell Elshalem <hilell.elshalem@gmail.com>
2021-12-20 23:29:58 +0200

    add controllers - title & description

---
+++ apps/back-end/controllers/posts.js
@@ -0,0 +1,45 @@
+
+
+const createPost = (req, res) => {
+    // create new post
+    // POST method
+    // /api/posts
+} 
+
+const getPost = (req, res) => {
+    // get post data by :postID param
+    // GET method
+    // /api/posts/:postId
+    // ** include all the post data, like:
+    //      comments,
+    //      likes,
+    //      usernames & images sorces
+    //      ....
+
+} 
+
+const deletePost = (req, res) => {
+    // delete post by :postId param
+    // DELETE method
+    // /api/posts/:postId
+} 
+
+const updatePost = (req, res) => {
+    // update post data by :postId param
+    // PUT method
+    // /api/posts/:postId
+} 
+
+const toggleLikePost = (req, res) => {
+    // add corrent user to post's likes list
+    // POST method
+    // /api/posts/:postId/like
+} 
+
+module.exports = {
+    createPost,
+    getPost,
+    deletePost,
+    updatePost,
+    toggleLikePost
+}
