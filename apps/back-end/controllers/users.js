e49e1f03d8550621f9284e56e899056fb936ee6a
Hilell Elshalem <hilell.elshalem@gmail.com>
2021-12-20 23:29:58 +0200

    add controllers - title & description

---
+++ apps/back-end/controllers/users.js
@@ -0,0 +1,57 @@
+
+
+const getUser = (req, res) => {
+    // get user data by :userID param
+    // GET method
+    // /api/users/:userId
+    // ** include all the user data, like:
+    //      posts,
+    //      additional data
+    //      ....
+
+}
+
+const toggleFollowUser = (req, res) => {
+    // add corrent user to user's following list by :userID param
+    // POST method
+    // /api/users/:userId/follow
+}
+
+const getUserPosts = (req, res) => {
+    // get user posts by :userID param
+    // GET method
+    // /api/users/:userId/posts
+    // ** include all the data, like:
+    //      likes,
+    //      comments,
+    //      usernames & images sorces
+    //      ....
+}
+
+const getMe = (req, res) => {
+    // get all user data
+    // GET method
+    // /api/me
+}
+
+const updateMe = (req, res) => {
+    // update user data
+    // POST method
+    // /api/me
+}
+
+const deleteMe = (req, res) => {
+    // delete user include all his data
+    // DELETE method
+    // /api/me
+}
+
+
+module.exports = {
+    getUser,
+    getUserPosts,
+    toggleFollowUser,
+    getMe,
+    updateMe,
+    deleteMe
+}
--- apps/back-end/routes/posts.js
+++ apps/back-end/routes/posts.js
@@ -1,7 +1,14 @@
 const { Router } = require('express');
+const {
+    createPost,
+    getPost,
+    deletePost,
+    updatePost,
+    toggleLikePost
+} = require('../controllers/posts');
 
 const postsRouter = Router();
-postsRouter.post('/api/posts/:postId', createPost);
+postsRouter.post('/api/posts', createPost);
 postsRouter.get('/api/posts/:postId', getPost);
 postsRouter.delete('/api/posts/:postId', deletePost);
 postsRouter.put('/api/posts/:postId', updatePost);
