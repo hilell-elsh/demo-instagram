a3166b19449816cdbb3adf710e003bda576fbacb
Hilell Elshalem <hilell.elshalem@gmail.com>
2022-01-17 16:29:10 +0200

    add get feed controller

--- apps/back-end/routes/feed.js
+++ apps/back-end/routes/feed.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
+const { getLimitParams } = require('../middlewares/limitParams')
 const  {getFeed } = require('../controllers/feed')
 
 const feedRouter = Router();
-feedRouter.get('/api/feed', getFeed);
+feedRouter.get('/api/feed', getLimitParams, getFeed);
 
 
 module.exports = feedRouter;
