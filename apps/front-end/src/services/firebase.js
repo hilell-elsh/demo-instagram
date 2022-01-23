6a783cba6afd1bc5206c2b047419c12ff4f6b493
Hilell Elshalem <hilell.elshalem@gmail.com>
2022-01-23 21:47:10 +0200

    rip

---
+++ apps/front-end/src/services/firebase.js
@@ -0,0 +1,16 @@
+// import { initializeApp } from 'firebase/app'
+// import { getAuth } from 'firebase/auth'
+
+// const firebaseConfig = {
+//     apiKey: 'AIzaSyC0tbPLFe2HtkSWG_XvmIjVQcqpB9erh_0',
+//     authDomain: 'kilogram-22cf9.firebaseapp.com',
+//     projectId: 'kilogram-22cf9',
+//     storageBucket: 'kilogram-22cf9.appspot.com',
+//     messagingSenderId: '47005673149',
+//     appId: '1:47005673149:web:212362b1d0e443df4139d4'
+// }
+
+// const app = initializeApp(firebaseConfig)
+
+// export const auth = getAuth(app)
+// export default app
