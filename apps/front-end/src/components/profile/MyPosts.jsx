bf16e27ed52e1701ebe715821169c296de73ce84
Hilell Elshalem <hilell.elshalem@gmail.com>
2022-01-23 21:45:57 +0200

    rip

--- apps/front-end/src/components/profile/MyPosts.jsx
+++ apps/front-end/src/components/profile/MyPosts.jsx
@@ -1,8 +1,37 @@
 import { useLocation } from 'react-router-dom'
+import { useState } from 'react'
 import Masonry from '@mui/lab/Masonry'
 import Stack from '@mui/material/Stack'
 
 import { getUser } from '../../services/user-data.js'
+import PostBanner from '../global/PostBanner'
+
+function PostStack({item, index}) {
+    const [isBanner, setIsBanner] = useState(false)
+
+    return (
+        <Stack 
+            key={index} 
+            style={{position: 'relative'}}
+            onMouseEnter={() => setIsBanner(true)}
+            onMouseLeave={() => setIsBanner(false)}
+            >
+                    <img
+                        src={`${item.img}?w=162&auto=format`}
+                        srcSet={`${item.img}?w=162&auto=format&dpr=2 2x`}
+                        alt={item.title}
+                        loading="lazy"
+                        style={{ borderBottomLeftRadius: 4,
+                            borderBottomRightRadius: 4,
+                            borderTopRightRadius: 4,
+                            borderTopLeftRadius: 4,
+                            cursor: "pointer"
+                        }}
+                    />
+                    {isBanner && <PostBanner />}
+                </Stack>
+    )
+}
 
 export default function MyPosts() {
     const location = useLocation()
@@ -22,24 +51,11 @@ export default function MyPosts() {
             posts = getUser().profile.posts.myPosts
     }
 
+
     return (
         <Masonry columns={3} spacing={1}>
             {posts.map((item, index) => (
-                <Stack key={index}>
-                    <img
-                        src={`${item.img}?w=162&auto=format`}
-                        srcSet={`${item.img}?w=162&auto=format&dpr=2 2x`}
-                        alt={item.title}
-                        loading="lazy"
-                        style={{
-                            borderBottomLeftRadius: 4,
-                            borderBottomRightRadius: 4,
-                            borderTopRightRadius: 4,
-                            borderTopLeftRadius: 4,
-                            cursor: 'pointer',
-                        }}
-                    />
-                </Stack>
+                <PostStack item={item} index={index}/>
             ))}
         </Masonry>
     )
