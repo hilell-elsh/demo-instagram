5ed00407e17178bda1901ca6240388b5434457fd
Shaked Shaul <shukeds@gmail.com>
2022-01-28 00:35:58 +0200

    wip

--- apps/front-end/src/components/FeedPost/PostMedia.jsx
+++ apps/front-end/src/components/FeedPost/PostMedia.jsx
@@ -1,10 +1,12 @@
 import CardMedia from '@mui/material/CardMedia'
 
-export default function PostMedia() {
+export default function PostMedia({ contentItem }) {
     return (
-        <CardMedia
+
+        
+        <CardMedia 
             component="img"
-            image="https://images.unsplash.com/photo-1546587348-d12660c30c50?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bmF0dXJhbHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80"
+            image={contentItem.images[0]}
             alt="Paella dish"
         />
     )
