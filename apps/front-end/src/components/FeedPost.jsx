dd12151c13a7a7cc141e0563418222dd35979439
Shaked Shaul <shukeds@gmail.com>
2022-01-28 00:44:00 +0200

    wip

--- apps/front-end/src/components/FeedPost.jsx
+++ apps/front-end/src/components/FeedPost.jsx
@@ -11,22 +11,27 @@ import PostCollapse from './FeedPost/PostCollapse'
 
 export default function Feed({contentItem}) {
 
+export default function Feed({ contentItem }) {
     const [expanded, setExpanded] = React.useState(false);
-    const {postHeader, postContent, postMedia} = React.useMemo(() => {
+    const { postHeader, postContent, postMedia } = React.useMemo(() => {
         return {
             postHeader: <PostHeader contentItem={contentItem} />,
             postContent: <PostContent contentItem={contentItem} />,
-            postMedia: <PostMedia contentItem={contentItem} />
-        }
-    }, [])
+            postMedia: <PostMedia contentItem={contentItem} />,
+        };
+    }, []);
 
     return (
         <Card sx={{ maxWidth: 1080, marginTop: '5px' }}>
             {postHeader}
             {postMedia}
             {postContent}
-            <PostActions contentItem={contentItem} expanded= {expanded} setExpanded= {setExpanded}  /> 
-            <PostCollapse expanded= {expanded}  />
+            <PostActions
+                contentItem={contentItem}
+                expanded={expanded}
+                setExpanded={setExpanded}
+            />
+            <PostCollapse expanded={expanded} />
         </Card>
     )
 }
