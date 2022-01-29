b110419419fc183b546372f829237bb5f156d43e
Shaked Shaul <shukeds@gmail.com>
2022-01-29 19:57:46 +0200

    wip

--- apps/front-end/vite.config.ts
+++ apps/front-end/vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
         outDir: "../back-end/static",
-        target: "esnext"
+        target: "esnext",
     },
   plugins: [react()],
   server: {
