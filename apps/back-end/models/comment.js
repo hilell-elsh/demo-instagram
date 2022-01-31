a0f864669ca78da0bb5f65a2aa559f68305dc5e4
מאור לכיש <mlachish1@gmail.com>
2022-01-31 23:40:37 +0200

    changed likes and comments models and started changing services and controllers accordingly

--- apps/back-end/models/comment.js
+++ apps/back-end/models/comment.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose')
 const ObjectId = mongoose.Schema.ObjectId
 
-const CommentSchema = new mongoose.Schema({
-    postId: { type: ObjectId, ref: 'Post', required: true },
+const UserCommentSchema = new mongoose.Schema({
     user: { type: ObjectId, ref: 'User', required: true },
     text: { type: String, required: true },
     createdDate: { type: Date, default: Date.now },
+})
+
+const CommentSchema = new mongoose.Schema({
+    postId: { type: ObjectId, ref: 'Post', required: true },
+    comments: [UserCommentSchema],
 })
 
 const CommentModel = mongoose.model('Comment', CommentSchema)
--- apps/back-end/models/like.js
+++ apps/back-end/models/like.js
@@ -4,8 +4,7 @@ const ObjectId = mongoose.Schema.ObjectId
 const LikeSchema = new mongoose.Schema({
     postId: { type: ObjectId, ref: 'Post', required: true },
     commentId: { type: ObjectId, ref: 'Comment' },
-    user: { type: ObjectId, ref: 'User', required: true },
-    createdDate: { type: Date, default: Date.now },
+    users: [{ type: ObjectId, ref: 'User', required: true }],
 })
 
 LikeSchema.index({ postId: true, commentId: true, user: true}, {unique: true})
