86cadbd1b5b4377576442d80556f79fe4e6c480b
Hilell Elshalem <hilell.elshalem@gmail.com>
2022-02-06 12:58:16 +0200

    add to write post component

--- apps/front-end/src/components/global/NewPostWrite.jsx
+++ apps/front-end/src/components/global/NewPostWrite.jsx
@@ -1,13 +1,21 @@
+import styled from 'styled-components'
 
+const NewPostData = styled.div`
+    display: flex;
+`
 
 export default function NewPostWrite({imagesUrls, imagesAmount}) {
     
     return (
         <>
-            <div className="postData">
-                <p>Write your post here, you have {imagesAmount} images</p>
-                <input type="text" placeholder="Write your post here" />
-            </div>
+            <NewPostData>
+                <form>
+                    <textarea name="text" rows="10" placeholder="Write your post..." />
+                    <input name="location" type="text" placeholder="location" />
+
+                    <button>Share</button>
+                </form>
+            </NewPostData>
             <p>{imagesUrls.join('\n')}</p>
         </>
     )
