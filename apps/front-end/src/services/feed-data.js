93e40ab2cf0e932d1e9a13791685667dd9e49583
Shaked Shaul <shukeds@gmail.com>
2022-02-14 19:11:17 +0200

    wip

--- apps/front-end/src/services/feed-data.js
+++ apps/front-end/src/services/feed-data.js
@@ -1,7 +1,6 @@
 import fetching from './basic-fetch'
 
-
-export async function getFeed(skip=0, limit=20) {
-    const path = `/api/feed?skip=${skip}&limit=${limit}`;
-    return await fetching({path})
+export function getFeed(skip = 0, limit = 20) {
+    const path = `/api/feed?skip=${skip}&limit=${limit}`
+    return fetching({ path })
 }
