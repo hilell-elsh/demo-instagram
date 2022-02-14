6545a1a4162befbe6a22348fceec2d1db1726d71
Shaked Shaul <shukeds@gmail.com>
2022-02-14 19:20:09 +0200

    wip

--- apps/front-end/src/components/FeedPost/PostActions/ActionLike.jsx
+++ apps/front-end/src/components/FeedPost/PostActions/ActionLike.jsx
@@ -2,23 +2,38 @@ import Chip from '@mui/material/Chip'
 import Tooltip from '@mui/material/Tooltip'
 import IconButton from '@mui/material/IconButton'
 import FavoriteIcon from '@mui/icons-material/Favorite'
+import { red } from '@mui/material/colors';
 
 import {toggleLikePost} from '../../../services/post-data'
+import { useState } from 'react'
 
 function likePost(contentItem) {
     toggleLikePost(contentItem._id)
     console.log("post", contentItem._id, 'liked'  )
 }
 export default function ActionLike({ contentItem}) {
+
+    const [isLiked, setIsLiked] = useState(contentItem.isLiked = false)
+    const [likesAmount, setLikesAmount] = useState(contentItem.likesAmount)  
+
     return (
         <Chip 
-            onClick={() => likePost(contentItem)}
+            onClick={() => {
+                likePost(contentItem)
+                if (isLiked) {
+                    setLikesAmount( likesAmount-1)
+                } else {
+                    setLikesAmount( likesAmount+1 )
+                }
+                setIsLiked(!isLiked)
+                }       
+            }
             icon={
             <Tooltip title="Like">
                 <IconButton aria-label="like">
-                    <FavoriteIcon  />
+                {isLiked ? <FavoriteIcon sx={{ color: red[500] }} /> : <FavoriteIcon  />}
                 </IconButton>
             </Tooltip>
-        } label={contentItem.likesAmount + " Likes"} variant="outlined" />
+        } label={likesAmount + " Likes"} variant="outlined" />
     )
 }
