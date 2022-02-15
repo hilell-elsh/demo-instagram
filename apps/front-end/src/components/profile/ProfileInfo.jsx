e9d7ab4eb4a86841a1b3a875512d374662a0c3d2
מאור לכיש <mlachish1@gmail.com>
2022-02-15 23:03:18 +0200

    added follow/unfollow button and connected it to server

--- apps/front-end/src/components/profile/ProfileInfo.jsx
+++ apps/front-end/src/components/profile/ProfileInfo.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 import Avatar from '@mui/material/Avatar'
@@ -6,8 +7,58 @@ import { AnimatePresence } from 'framer-motion'
 import { Username, Info, Button, Count } from './ProfileStyle'
 import { modalOpen } from '../../store/modal'
 import FollowerModal from './FollowerModal'
+import { toggleFollowUser, checkIsFollowing } from '../../services/user-data'
 
-export default function ProfileInfo({ user }) {
+export default function ProfileInfo({ user, usernameParam }) {
+    const myUsername = useSelector(
+        (state) => state.user.user.userBasicData.username
+    )
+    const [isFollowing, setIsFollowing] = useState(false)
+    const [followersCount, setFollowersCount] = useState(0)
+
+    useEffect(async () => {
+        await setIsFollowing(checkIsFollowing(user._id))
+        setFollowersCount(user.additionalData.followers)
+    }, [user])
+
+    async function renderInfoButton() {
+        if (myUsername === usernameParam) {
+            return (
+                <Link to="/settings">
+                    <Button>Edit Profile</Button>
+                </Link>
+            )
+        } else if (isFollowing) {
+            return (
+                <Button
+                    onClick={() => {
+                        toggleFollowUser(user._id)
+                        setIsFollowing(false)
+                        setFollowersCount(followersCount - 1)
+                    }}
+                >
+                    Unfollow
+                </Button>
+            )
+        } else {
+            return (
+                <Button
+                    onClick={() => {
+                        toggleFollowUser(user._id)
+                        setIsFollowing(true)
+                        setFollowersCount(followersCount + 1)
+                    }}
+                >
+                    Follow
+                </Button>
+            )
+        }
+    }
+    const [infoButton, setInfoButton] = useState(null)
+    useEffect(async () => {
+        setInfoButton(await renderInfoButton())
+    }, [user, isFollowing])
+
     const userProfilePic = user.userBasicData.profileImageSrc
     const userAvatar = (
         <Avatar
@@ -16,6 +67,7 @@ export default function ProfileInfo({ user }) {
             sx={{ width: 164, height: 164 }}
         />
     )
+
     const isModal = useSelector((state) => state.modal.isModal)
     const title = useSelector((state) => state.modal.title)
     const dispatch = useDispatch()
@@ -28,7 +80,7 @@ export default function ProfileInfo({ user }) {
                 {user.posts.postsAmount} <span>posts</span>
             </Count>
             <Count>
-                {user.additionalData.followers}{' '}
+                {followersCount}{' '}
                 <span onClick={() => dispatch(modalOpen())}>followers</span>
             </Count>
             <Count>
@@ -40,16 +92,9 @@ export default function ProfileInfo({ user }) {
                 exitBeforeEnter
                 onExitComplete={() => null}
             >
-                {isModal && (
-                    <FollowerModal
-                        title={title}
-                        userId={user._id}
-                    />
-                )}
+                {isModal && <FollowerModal title={title} userId={user._id} />}
             </AnimatePresence>
-            <Link to="/settings">
-                <Button>Edit Profile</Button>
-            </Link>
+            {infoButton}
         </Info>
     )
 }
