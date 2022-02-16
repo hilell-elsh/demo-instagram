0dcc3a64ca6a6dee755828d868bc4266874e2e28
Hilell Elshalem <hilell.elshalem@gmail.com>
2022-02-16 17:01:02 +0200

    followers-fix rebase

--- apps/front-end/src/components/FeedPost/PostMedia.jsx
+++ apps/front-end/src/components/FeedPost/PostMedia.jsx
@@ -1,10 +1,10 @@
 import CardMedia from '@mui/material/CardMedia'
 
-export default function PostMedia({ contentItem }) {
+export default function PostMedia() {
     return (
         <CardMedia 
             component="img"
-            image={contentItem.images[0]}
+            image="https://images.unsplash.com/photo-1546587348-d12660c30c50?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bmF0dXJhbHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80"
             alt="Paella dish"
         />
     )
